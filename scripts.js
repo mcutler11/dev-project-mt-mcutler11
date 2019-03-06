871d5984a47d54816820d7a28f6754f826515c3b
Michael Cutler <michael.cutler11@yahoo.com>
2019-03-06 13:50:26 -0600

    Initialize the object, then assign values, then calculate the monthly payment.

--- scripts.js
+++ scripts.js
@@ -10,18 +10,32 @@
  * 1. LISTEN for and HANDLE the 'submit' event.
  * 2. QUERY the DOCUMENT for the <form> fields and store these values in variables.
  * 2a. Actually first, initialize an Object (as var).
- * 2b. The object shall be made up of three (for now)
+ * 2b. The object shall be made up of three (for now) fields:  amount, interest, and period.
+ * 3.
  */
 
 // First, initialize the form, the submitHandler, and the object
-const monthlyPlan = new Object();
-monthlyPlan.amount;
-monthlyPlan.interest;
-monthlyPlan.period;
 
-const form = document.querySelector("form");
 const submitHandler = function submitHandler(e) {
-  e.PreventDefault(); // Prevents the default from following its default.  Duh.
+  e.preventDefault(); // Prevents the default from following its default.  Duh.
 
-  // NEXT:  Try to piece together a plan for the next phase.
+  // Initialize the monthlyPlan object.
+  const monthlyPlan = {
+    amount: document.querySelector("#amount").value,
+    interest: document.querySelector("#interest").value / 100,
+    period: document.querySelector("#period").value
+  };
+
+  const pv = monthlyPlan.amount;
+  const r = monthlyPlan.interest;
+  const n = monthlyPlan.period;
+
+  const p = (r * pv) / (1 - (1 + r) ** -n);
+  // Find a way to allow the value of "interest" to be a double (decimal).
+
+  console.log(monthlyPlan); // SUCCESS.
+  console.log(p);
 };
+
+// Here, we listen for and handle the 'submit' event.
+document.querySelector("form").addEventListener("submit", submitHandler);
